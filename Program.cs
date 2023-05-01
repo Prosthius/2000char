b47cc610202925b452a819de8b086d6c9e8cd6dd
Prosthius <mail@callumhopkins.au>
2023-05-01 19:48:33 +1000

    TODO - add error handling

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string input = File.ReadAllText("./input/input.txt");
+int i = 1;
+
+List<string> output = SplitString(input, 2000);
+
+foreach (string block in output)
+{
+	File.WriteAllText($"./output/output{i}.txt", block);
+	i++;
+}
+
+List<string> SplitString(string str, int blockSize)
+{
+    List<string> output = new List<string>();
+    for (int i = 0; i < str.Length; i += blockSize)
+    {
+        if (i + blockSize > str.Length) blockSize = str.Length - i;
+        output.Add(str.Substring(i, blockSize));
+    }
+    return output;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+Paste the text on one line into ./input/input.txt, the text will be output into several files in ./output,
+each consisting of up to 2000 characters, including whitespace.
